9ee6f4d00b23b4124f7d4d691878fbd4474c31cd
ZorphDark <zorphdark@gmail.com>
2016-02-24 13:20:30 -0800

    new theme based on http://html5up.net/identity

--- gruntfile.js
+++ gruntfile.js
@@ -1,189 +1,188 @@
 /*!
- * Alejandro Cuba (http://www.alejandrocuba.com/)
- * (c) 2015 Alejandro Cuba Ruiz's website
+ * Alejandro Cuba (http://alejandrocuba.com/)
+ * (c) 2016 Alejandro Cuba Ruiz's website
  * <> by ZorphDark
  */
 
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		path: {
-			base: './',
-			sources: 'sources/',
-			assets: 'dist/assets/',
-			css: {
-				source: '<%= path.sources %>css/',
-				dist: '<%= path.assets %>css/'
-			},
-			js: {
-				source: '<%= path.sources %>js/',
-				dist: '<%= path.assets %>js/'
-			},
-			html: {
-				source: '<%= path.sources %>html/',
-				dist: 'dist/html/'
-			}
-		},
-		banner: '/*!\n' +
-			' * <%= pkg.name %> (<%= pkg.homepage %>)\n' +
-			' * (c) <%= grunt.template.today("yyyy") %> <%= pkg.description %>\n' +
-			' */\n\n',
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    path: {
+      base: './',
+      sources: 'sources/',
+      assets: 'dist/assets/',
+      css: {
+        source: '<%= path.sources %>css/',
+        dist: '<%= path.assets %>css/'
+      },
+      js: {
+        source: '<%= path.sources %>js/',
+        dist: '<%= path.assets %>js/'
+      },
+      html: {
+        source: '<%= path.sources %>html/',
+        dist: 'dist/html/'
+      }
+    },
+    banner: '/*!\n' +
+      ' * <%= pkg.name %> (<%= pkg.homepage %>)\n' +
+      ' * (c) <%= grunt.template.today("yyyy") %> <%= pkg.description %>\n' +
+      ' */\n\n',
 
-		less: {
-			options: {
-				strictImports: true,
-				ieCompat: true,
-				cleancss: true
-			},
-			dist: {
-				files: {
-					'<%= path.css.source %>compiled.min.css' : '<%= path.css.source %>alejandrocuba.less'
-				}
-			}
-		},
+    // less: {
+    //   options: {
+    //     strictImports: true,
+    //     ieCompat: true,
+    //     cleancss: true
+    //   },
+    //   dist: {
+    //     files: {
+    //       '<%= path.css.source %>compiled.min.css': '<%= path.css.source %>alejandrocuba.less'
+    //     }
+    //   }
+    // },
 
-		autoprefixer: {
-			options: {
-				browsers: ['last 30 versions', 'bb 10', 'android 3', 'ie 8', 'ie 9']
-			},
-			compiled: {
-				src: '<%= path.css.source %>compiled.min.css',
-				dest: '<%= path.css.source %>compiled.min.css'
-			}
-		},
+    sass: {
+      dist: {
+        files: {
+          '<%= path.css.source %>compiled.min.css':'<%= path.css.source %>alejandrocuba.scss'
+        }
+      }
+    },
 
-		// uglify: {
-		// 	options: {
-					mangle: false,
-		// 		preserveComments: 'some'
-		// 	},
-		// 	dist: {
-		// 		files: {
-		// 			'<%= path.js.source %>compiled.min.js': '<%= path.js.source %>base.js'
-		// 		}
-		// 	}
-		// },
+    autoprefixer: {
+      options: {
+        browsers: ['last 30 versions', 'bb 10', 'android 3', 'ie 8', 'ie 9']
+      },
+      compiled: {
+        src: '<%= path.css.source %>compiled.min.css',
+        dest: '<%= path.css.source %>compiled.min.css'
+      }
+    },
 
-		concat : {
-			css: {
-				files: {
-					'<%= path.css.dist %>alejandrocuba.min.css': [
-						'<%= path.css.source %>vendor/bootstrap/bootstrap.min.css',
-						'<%= path.css.source %>vendor/font-awesome/font-awesome.min.css',
-						'<%= path.css.source %>compiled.min.css'
-					]
-				}
-			},
-			// js: {
-			// 	files: {
-			// 		'<%= path.js.dist %>alejandrocuba.min.js': [
-			// 			'<%= path.js.source %>vendor/jquery.min.js',
-			// 			'<%= path.js.source %>vendor/bootstrap.min.js',
-			// 			'<%= path.js.source %>compiled.min.js'
-			// 		]
-			// 	}
-			// }
-		},
+    uglify: {
+      options: {
+        mangle: false,
+        preserveComments: 'some'
+      },
+      dist: {
+        files: {
+          '<%= path.js.source %>compiled.min.js': '<%= path.js.source %>base.js'
+        }
+      }
+    },
 
-		uncss: {
-			dist: {
-				options: {
-					ignoreSheets : [/fonts.googleapis/],
-				},
-				files: {
-					'<%= path.css.dist %>alejandrocuba.min.css': ['<%= path.html.dist %>**/*.html']
-				}
-			}
-		},
+    concat: {
+      css: {
+        files: {
+          '<%= path.css.dist %>alejandrocuba.min.css': [
+            '<%= path.css.source %>vendor/bootstrap/bootstrap.min.css',
+            '<%= path.css.source %>vendor/font-awesome/font-awesome.min.css',
+            '<%= path.css.source %>compiled.min.css'
+          ]
+        }
+      },
+      js: {
+        files: {
+          '<%= path.js.dist %>alejandrocuba.min.js': [
+            // '<%= path.js.source %>vendor/jquery.min.js',
+            // '<%= path.js.source %>vendor/bootstrap.min.js',
+            '<%= path.js.source %>compiled.min.js'
+          ]
+        }
+      }
+    },
 
- 		cssmin: {
- 			dist: {
- 				files: {
- 					'<%= path.css.dist %>alejandrocuba.min.css' : '<%= path.css.dist %>alejandrocuba.min.css'
- 				}
- 			}
- 		},
+    uncss: {
+      dist: {
+        options: {
+          ignoreSheets: [/fonts.googleapis/],
+        },
+        files: {
+          '<%= path.css.dist %>alejandrocuba.min.css': ['<%= path.html.dist %>**/*.html']
+        }
+      }
+    },
 
-		usebanner: {
-			options: {
-				banner: '<%= banner %>'
-			},
-			css: {
-				files: {
-					src: [ '<%= path.css.dist %>alejandrocuba.min.css' ]
-				}
-			},
-			// js: {
-			// 	files: {
-			// 		src: [ '<%= path.js.dist %>alejandrocuba.min.js' ]
-			// 	}
-			// }
-		},
+    cssmin: {
+      dist: {
+        files: {
+          '<%= path.css.dist %>alejandrocuba.min.css': '<%= path.css.dist %>alejandrocuba.min.css'
+        }
+      }
+    },
 
-		jekyll: {
-			build: {
-				options: {
-					config: '<%= path.base %>_config.yml',
-					src: '<%= path.html.source %>',
-					dest: '<%= path.html.dist %>'
-				}
-			}
-		},
+    usebanner: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      css: {
+        files: {
+          src: ['<%= path.css.dist %>alejandrocuba.min.css']
+        }
+      },
+      js: {
+        files: {
+          src: ['<%= path.js.dist %>alejandrocuba.min.js']
+        }
+      }
+    },
 
- 		htmllint: {
- 			options: {
- 				force: true,
- 				maxerr: 50,
- 				'id-class-style': false,
- 				'attr-name-style': false,
- 				'line-end-style': false,
- 			},
- 			src: ['<%= path.html.dist %>**/*.html']
- 		},
+    jekyll: {
+      build: {
+        options: {
+          config: '<%= path.base %>_config.yml',
+          src: '<%= path.html.source %>',
+          dest: '<%= path.html.dist %>'
+        }
+      }
+    },
 
- 		bootlint: {
- 			options: {
- 				stoponerror: false,
- 				relaxerror: ['W005']
- 			},
- 			files: ['<%= path.html.dist %>**/*.html']
- 		},
+    // 	htmllint: {
+    // 		options: {
+    // 			force: true,
+    // 			maxerr: 50,
+    // 			'id-class-style': false,
+    // 			'attr-name-style': false,
+    // 			'line-end-style': false,
+    // 		},
+    // 		src: ['<%= path.html.dist %>**/*.html']
+    // 	},
 
-		watch: {
-			options: { livereload: true },
-			css: {
-				files: ['<%= path.css.source %>**/*.less'],
-				tasks: ['css']
-			},
-			// js: {
-			// 	files: ['<%= path.js.source %>**/*.js',
-			// 			'! <%= path.js.source %>compiled.min.js'
-			// 	],
-			// 	tasks: ['js']
-			// },
-			html: {
-				files: ['<%= path.html.source %>**/*.*'],
-				tasks: ['html', 'css']
-			}
-		}
-	});
+    watch: {
+      options: {
+        livereload: true
+      },
+      css: {
+        files: ['<%= path.css.source %>**/*.scss'],
+        tasks: ['css']
+      },
+      js: {
+        files: ['<%= path.js.source %>**/*.js',
+          '! <%= path.js.source %>compiled.min.js'
+        ],
+        tasks: ['js']
+      },
+      html: {
+        files: ['<%= path.html.source %>**/*.*'],
+        tasks: ['html', 'css']
+      }
+    }
+  });
 
-	require('time-grunt')(grunt);
+  require('time-grunt')(grunt);
 
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-autoprefixer');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-uncss');
-	grunt.loadNpmTasks('grunt-banner');
-	grunt.loadNpmTasks('grunt-jekyll');
-	grunt.loadNpmTasks('grunt-htmllint');
-	grunt.loadNpmTasks('grunt-bootlint');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-banner');
+  grunt.loadNpmTasks('grunt-jekyll');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('html', ['jekyll', 'htmllint', 'bootlint']);
-	grunt.registerTask('css', ['less', 'autoprefixer', 'concat:css', /*'uncss', 'cssmin',*/ 'usebanner:css']);
-	// grunt.registerTask('js', ['uglify', 'concat:js', 'usebanner:js']);
-	grunt.registerTask('default', ['html', 'css']);
+  grunt.registerTask('html', ['jekyll', /*'htmllint', 'bootlint'*/ ]);
+  grunt.registerTask('css', ['sass', 'autoprefixer', 'concat:css', /*'uncss', 'cssmin',*/ 'usebanner:css']);
+  grunt.registerTask('js', ['uglify', 'concat:js', 'usebanner:js']);
+  grunt.registerTask('default', ['html', 'css']);
 };
