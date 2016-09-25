b494da34ca40ba59bb249f21c8390f743a235609
ZorphDark <zorphdark@gmail.com>
2016-09-24 23:15:13 -0700

    .portrait

--- sources/js/scripts.js
+++ sources/js/scripts.js
@@ -4,18 +4,18 @@ $(window).load(function() {
   $('.page-loader').fadeOut('slow');
 });
 
-
-
-/* ------------ *
- * Module fade *
-/* ---------- */
-var $window = $(window);
-$(".module-fade").each(function() {
-  var self = $(this);
-  $window.scroll(function() {
-    var topScroll = $(document).scrollTop();
-    if (topScroll <= self.height()) {
-      self.css('opacity', (1 - topScroll/self.height() * 1.1));
-    }
+$(function () {
+  /* ------------ *
+   * Module fade *
+  /* ---------- */
+  var $window = $(window);
+  $(".module-fade").each(function() {
+    var self = $(this);
+    $window.scroll(function() {
+      var topScroll = $(document).scrollTop();
+      if (topScroll <= self.height()) {
+        self.css('opacity', (1 - topScroll/self.height() * 1.1));
+      }
+    });
   });
 });
